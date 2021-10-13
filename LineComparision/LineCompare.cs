94a9fb0c0e8ee32eb32626338371281ae815f47a
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-10-13 12:33:29 +0530

    [comparing lines in class]

---
+++ LineComparision/LineCompare.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    class LineCompare
+    {
+
+        int x1, x2, y1, y2;
+        int p1, p2, q1, q2;
+        public int getElement(int x1,int x2,int y1,int y2,int p1,int p2,int q1,int q2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+            this.p1 = p1;
+            this.p2 = p2;
+            this.q1 = q1;
+            this.q2 = q2;
+            return 0;
+        }
+        public int checkLine()
+        {
+
+            double line1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) * 1.0);
+            Console.WriteLine("the length of line is:{0}", line1);
+
+
+            double line2 = Math.Sqrt(Math.Pow(p2 - p1, 2) + Math.Pow(q2 - q1, 2) * 1.0);
+            Console.WriteLine("the length of line is:{0}", line2);
+
+            if (line1 == line2)
+            {
+                Console.WriteLine("bothline are equal");
+            }
+            else if (line1 > line2)
+            {
+                Console.WriteLine("line1 is greater");
+            }
+            else
+            {
+                Console.WriteLine("line2 is greater");
+            }
+
+            return 0;
+        }
+
+    }
+}
--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -19,12 +19,6 @@ namespace LineComparision
             y1 = Convert.ToInt32(Console.ReadLine());
             y2 = Convert.ToInt32(Console.ReadLine());
             
-
-            double line1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) * 1.0);
-            Console.WriteLine("the length of line is:{0}", line1);
-           
-
-
             Console.WriteLine("enter the 2nd line  x coordinate");
             p1 = Convert.ToInt32(Console.ReadLine());
             p2 = Convert.ToInt32(Console.ReadLine());
@@ -32,21 +26,9 @@ namespace LineComparision
             q1 = Convert.ToInt32(Console.ReadLine());
             q2 = Convert.ToInt32(Console.ReadLine());
 
-            double line2 = Math.Sqrt(Math.Pow(p2 - p1, 2) + Math.Pow(q2 - q1, 2) * 1.0);
-            Console.WriteLine("the length of line is:{0}", line2);
-            
-            if (line1 == line2)
-            {
-                Console.WriteLine("bothline are equal");
-            }
-            else if (line1 > line2)
-            {
-                Console.WriteLine("line1 is greater");
-            }
-            else
-            {
-                Console.WriteLine("line2 is greater");
-            }
+            LineCompare line = new LineCompare();
+            line.getElement(x1, x2, y1, y2, p1, p2, q1, q2);
+            line.checkLine();
 
             Console.ReadLine();
 
